a2e57cdb3c056d68dc11c71c0fdaadeb2c3bccd4
Aaron Chu <xweichu@hotmail.com>
2019-02-09 16:20:40 -0800

    add MyBasket and test

---
+++ MyBasket.py
@@ -0,0 +1,113 @@
+class MyBasket:
+
+    def __init__(self, branchPath = [], number = 0, offset = 0, size = 1, startEntry = 0, endEntry = 0, accessTime = -1):
+        self._branchPath = branchPath
+        self._number = number
+        self._offset = offset
+        self._size = size
+        self._startEntry = startEntry
+        self._endEntry = endEntry
+        self._accessTime = accessTime
+
+    def getBranchPath(self):
+        return self._branchPath
+    
+    def setBranchPath(self,branchPath):
+        if(branchPath == NULL):
+            print("Branch Path can't be null!")
+            return
+        self._branchPath = branchPath
+    
+    def getNumber(self):
+        return self._number
+    
+    def setNumber(self,number):
+        if(number < 0):
+            print("Number can't be negative!")
+            return
+        self._number = number
+
+    def getOffset(self):
+        return self._offset
+    
+    def setOffset(self,offset):
+        if(offset < 0):
+            print("Offset can't be negative!")
+            return
+        self._offset = offset
+    
+    def getSize(self):
+        return self._size
+    
+    def setSize(self,size):
+        if(size < 0):
+            print("Size can't be negative!")
+            return
+        self._size = size
+
+    def getStartEntry(self):
+        return self._startEntry
+    
+    def setStartEntry(self,startEntry):
+        if(startEntry < 0):
+            print("Start Entry can't be negative!")
+            return
+        self._startEntry = startEntry
+
+    def getEndEntry(self):
+        return self._endEntry
+    
+    def setEndEntry(self,endEntry):
+        if(endEntry < 0):
+            print("End Entry can't be negative!")
+            return
+        self._endEntry = endEntry
+    
+    def getAccessTime(self):
+        return self._accessTime
+    
+    def setAccessTime(self,accessTime):
+        if(accessTime < 0):
+            print("Access time can't be negative!")
+            return
+        self._accessTime = accessTime
+
+    def __gt__(self, other):
+        return self._offset > other._offset
+
+    def __lt__(self, other):
+        return self._offset < other._offset
+    
+    def __eq__(self, other):
+        return self._offset ==  other._offset
+    
+    def toString(self):
+        basketPath = ''
+        for i in range(len(self._branchPath)):
+            path = + self._branchPath[i] + ','
+        return basketPath + str(self._number) + ',' + str(self._offset) + ',' + str(self._size) + ',' + str(self._startEntry) + ',' + str(self._endEntry) + ',' + str(self._accessTime)
+    
+    def save(self, outFileHandler):
+        outFileHandler.write('Basket=' + str(self._number) + ',' + str(self._offset) + ',' + str(self._size) + ',' + str(self._startEntry) + ',' + str(self._endEntry) + ',' + str(self._accessTime) + '\n')
+        outFileHandler.flush()
+
+
+    def restore(self, inputFileHandler):
+        line = inputFileHandler.readline()
+        line = line.split('=')
+        line = line[1]
+        basket = line.split(',')
+        self.setBranchPath([])
+        self.setNumber(basket[0])
+        self.setOffset(basket[1])
+        self.setSize(basket[2])
+        self.setStartEntry(baske[3])
+        self.setEndEntry(basket[4])
+        
+        if(len(basket) == 6):
+            self.setAccessTime(basket[5])
+        else:
+            self.setAccessTime(-1)
+
+
+    
---
+++ test.py
@@ -0,0 +1,4 @@
+import MyBasket
+
+mb = MyBasket.MyBasket()
+print(mb.toString())
